5b00b341a9f3db682c7d15272990e792d0608117
nwcfafnir <nwc_fafnir@mail.ru>
2012-04-01 14:44:58 +0300

    Initials commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoPp::Application.config.secret_token = '7b4cc02af523dde730519dc5f3c0db833a16b10d425ebd23b46f1668b62e6e57c82b15196c9981c7ecd59c250d26ee390fd33439a9f775e47f222dc1bdad396c'
